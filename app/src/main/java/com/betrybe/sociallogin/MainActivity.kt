1cf24c4bf1de4c5d9bc2f6af24a49aee6c5efaf2
Rodrigo <rodrigomartins.agro@gmail.com>
2024-04-17 17:00:37 -0300

    Iniciando o projeto

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    // Começando o projeto
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
