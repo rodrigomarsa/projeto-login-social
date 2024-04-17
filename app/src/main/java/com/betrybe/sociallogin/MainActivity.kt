68b23a30414151fa848daa8b6af7508620eb9adf
Rodrigo <rodrigomartins.agro@gmail.com>
2024-04-17 17:29:37 -0300

    Resolução Requisito 01

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    // Começando o projeto
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
