891ce89f680243f411770a224f203822f1dda06e
Rodrigo <rodrigomartins.agro@gmail.com>
2024-04-19 19:10:45 -0300

    Resolução Requisito 07

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.betrybe.sociallogin
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.betrybe.sociallogin.R.id.login_button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private val emailInput:TextInputEditText by lazy { findViewById(R.id.email_text_input_layout) }
+    private val passwordInput:TextInputEditText by lazy { findViewById(R.id.password_text_input_layout) }
+    private val loginButton:Button by lazy { findViewById(login_button) }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        emailInput.addTextChangedListener { enableButton() }
+        passwordInput.addTextChangedListener { enableButton() }
+    }
+
+    private fun enableButton() {
+        val email = emailInput.text.toString().trim()
+        val password = passwordInput.text.toString().trim()
+
+        loginButton.isEnabled = email.length >= 1 && password.length >= 1
     }
 }
