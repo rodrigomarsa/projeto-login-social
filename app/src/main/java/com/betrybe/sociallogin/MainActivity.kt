f484c07551a5950ed49f92d9761863d242d65acb
Rodrigo <rodrigomartins.agro@gmail.com>
2024-04-20 11:17:41 -0300

    Resolução Requisito 08

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.betrybe.sociallogin.R.id.login_button
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
     private val emailInput:TextInputEditText by lazy { findViewById(R.id.email_input) }
+    private val emailLayout:TextInputLayout by lazy { findViewById(R.id.email_text_input_layout) }
     private val passwordInput:TextInputEditText by lazy { findViewById(R.id.password_input) }
     private val loginButton:Button by lazy { findViewById(login_button) }
 
@@ -20,6 +22,20 @@ class MainActivity : AppCompatActivity() {
 
         emailInput.addTextChangedListener { enableButton() }
         passwordInput.addTextChangedListener { enableButton() }
+
+        loginButton.setOnClickListener {
+            val email = emailInput.text.toString()
+            if (isValidEmail(email)) {
+                emailLayout.error = null
+            } else {
+                emailLayout.error = "Email inválido"
+            }
+        }
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        val emailPattern = "[a-zA-Z0-9.]+@[a-zA-Z]+.[a-zA-Z]+"
+        return email.matches(emailPattern.toRegex())
     }
 
     private fun enableButton() {
